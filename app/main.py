bfc716dad0f8f8c421bc3c281ef51e565c495429
Francisco Giana <gianafrancisco@gmail.com>
2021-11-27 15:45:53 -0300

    added health check
    Signed-off-by: Francisco Giana <gianafrancisco@gmail.com>

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import Depends, FastAPI
 from fastapi.security import OAuth2PasswordBearer
 
-from .routers import users, stocks
+from .routers import users, stocks, health
 
 app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(stocks.router)
+app.include_router(health.router)
