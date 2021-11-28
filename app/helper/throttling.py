12db94f819ff556408eeb8824e03c54e5353cc8d
Francisco Giana <gianafrancisco@gmail.com>
2021-11-28 11:00:33 -0300

    implement Generic Cell Rate Algorithm to rate-limit the API requests
    Signed-off-by: Francisco Giana <gianafrancisco@gmail.com>

--- app/helper/throttling.py
+++ app/helper/throttling.py
@@ -1,19 +1,74 @@
-from typing import Counter
+from datetime import datetime, timedelta
+from typing import Dict
+
 from fastapi.exceptions import HTTPException
 from starlette.requests import Request
 from starlette.status import HTTP_429_TOO_MANY_REQUESTS
 
+class ThrottlingException(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=HTTP_429_TOO_MANY_REQUESTS,
+            detail="Too Many Rquest",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+
+class RateLimit:
+    def __init__(self, count: int, period: timedelta = timedelta(0, 60)) -> None:
+        self.count = count
+        self.period = period
+
+    @property
+    def inverse(self) -> float:
+        return self.period.total_seconds() / self.count
+
+
+class gcra:
+    """
+    Generic Cell Rate Algorithm
+    Reference: https://smarketshq.com/implementing-gcra-in-python-5df1f11aaa96
+    """
+
+    def get_tat(self, key: str) -> datetime:
+        # This should return a previous tat for the key or the current time.
+        pass
+        
+    def set_tat(self, key: str, tat: datetime) -> None:
+        pass
+
+    def update(self, key: str, limit: RateLimit) -> bool:
+        now = datetime.utcnow()
+        tat = max(self.get_tat(key), now)
+        separation = (tat - now).total_seconds()
+        max_interval = limit.period.total_seconds() - limit.inverse
+        if separation > max_interval:
+            reject = True
+        else:
+            reject = False
+            new_tat = max(tat, now) + timedelta(seconds=limit.inverse)
+            self.set_tat(key, new_tat)
+        return reject
+
+
+class gcraMemory(gcra):
+
+    def __init__(self):
+        self.sessions: Dict[str, datetime] = {}
+
+    def get_tat(self, key: str) -> datetime:
+        if key not in self.sessions:
+            return datetime.now()
+        return self.sessions[key]
+
+    def set_tat(self, key: str, tat: datetime) -> None:
+        self.sessions[key] = tat
+
 class Throttling():
 
-    counter = 10
+    def __init__(self, rt: RateLimit = RateLimit(2, timedelta(0, 60, 0, 0)), gcra: gcra = gcraMemory()) -> None:
+        self.ratelimit = rt
+        self.gcra = gcra
 
     async def __call__(self, request: Request):
-        if Throttling.counter == 0:
-            raise HTTPException(
-                status_code=HTTP_429_TOO_MANY_REQUESTS,
-                detail="Not authenticated",
-                headers={"WWW-Authenticate": "Bearer"},
-            )
-        Throttling.counter -= 1
-
-throttling = Throttling()
+        if self.gcra.update("gianafrancisco", self.ratelimit):
+            raise ThrottlingException()
--- app/routers/stocks.py
+++ app/routers/stocks.py
@@ -1,10 +1,12 @@
+from datetime import timedelta
 import requests
 from fastapi import APIRouter, Depends
 
 from app.config import API_KEY, API_URL
 from app.helper.auth import oauth2_scheme
-from app.helper.throttling import throttling
+from app.helper.throttling import Throttling, RateLimit
 
+throttling = Throttling(rt=RateLimit(4))
 
 router = APIRouter(dependencies=[Depends(oauth2_scheme), Depends(throttling)])
 
